513317b6a2bc5e6524fb343acbf64c8900759132
Oda <odairvaz>
2021-05-22 14:23:27 +0200

    add exercise isEverywhere

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -252,12 +252,28 @@ public class Exer01 {
 		}
 		return (!has1 || !has4);
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/* We'll say that a value is "everywhere" in an array if for every pair of 
+	 * adjacent elements in the array, at least one of the pair is that value. 
+	 * Return true if the given value is everywhere in the array.
+	 * 1
+	 * isEverywhere([1, 2, 1, 3], 1) → true
+	 * isEverywhere([1, 2, 1, 3], 2) → false
+	 * isEverywhere([1, 2, 1, 3, 4], 1) → false
+	 */
+	public static boolean isEverywhere(int[] nums, int val) {
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] != val && nums[i + 1] != val)
+				return false;
+		}
+		return true;
+	}	
 
 	
-
 	public static void main(String[] args) {
-		int[] nums = {2, 3, 4};
-		System.out.println(no14(nums));
+		int[] nums = {2, 2, 1, 3};
+		System.out.println(isEverywhere(nums, 1));
 	}
 
 }
