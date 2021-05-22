e0d4d711f7786cfbcbaabb44b4598fe044ed5339
Oda <odairvaz>
2021-05-22 14:56:59 +0200

    add exercise either24

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -268,12 +268,36 @@ public class Exer01 {
 				return false;
 		}
 		return true;
-	}	
+	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given an array of ints, return true if the array 
+	 * contains a 2 next to a 2 or a 4 next to a 4, but not both.
+	 * 
+	 * either24([1, 2, 2]) → true
+	 * either24([4, 4, 1]) → true
+	 * either24([4, 4, 1, 2, 2]) → false
+	 */
+	public static boolean either24(int[] nums) {
+		boolean has22 = false;
+		boolean has44 = false;
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 2 && nums[i + 1] == 2)
+				has22 = true;
+
+			if (nums[i] == 4 && nums[i + 1] == 4)
+				has44 = true;
+		}
+		return has22 != has44;
+	}
+
 
 	
 	public static void main(String[] args) {
-		int[] nums = {2, 2, 1, 3};
-		System.out.println(isEverywhere(nums, 1));
+		int[] nums = {1, 2, 3, 4};
+		System.out.println(either24(nums));
 	}
 
 }
