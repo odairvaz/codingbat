6976cf76dac0fc6e6fda0f48f4e444c362d4d474
Oda <odairvaz>
2021-05-24 12:57:44 +0200

    add exercise has77

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -312,13 +312,32 @@ public class Exer01 {
 		return count;
 	}
 
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given an array of ints, return true if the array contains two 7's next to each other, 
+	 * or there are two 7's separated by one element, such as with {7, 1, 7}.
+	 * 
+	 * has77([1, 7, 7]) → true
+	 * has77([1, 7, 1, 7]) → true
+	 * has77([1, 7, 1, 1, 7]) → false
+	 */
+	public static boolean has77(int[] nums) {
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 7 && nums[i + 1] == 7)
+				return true;
+			if (i < nums.length - 2 && nums[i] == 7 && nums[i + 2] == 7)
+				return true;
+		}
+		return false;
+	}
+
 
 
 	
 	public static void main(String[] args) {
 		int[] nums1 = {1, 2, 3};
 		int[] nums2 = {2, 3, 3};
-		System.out.println(matchUp(nums1, nums2));
+		System.out.println(has77(nums1));
 	}
 
 }
