b1053942756c90f52972166c34b4169e75a2daef
Oda <odairvaz>
2021-05-26 22:03:29 +0200

    add exercise modThree

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -349,13 +349,33 @@ public class Exer01 {
 		return false;
 	}
 
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given an array of ints, return true if the array contains either 
+	 * 3 even or 3 odd values all next to each other.
+	 * 
+	 * modThree([2, 1, 3, 5]) → true
+	 * modThree([2, 1, 2, 5]) → false
+	 * modThree([2, 4, 2, 5]) → true
+	 */
+	public static boolean modThree(int[] nums) {
+		if(nums.length < 3) return false;
+		for (int i = 0; i < nums.length - 2; i++) {
+			//if(nums[i] % 2 == nums[i+1] % 2 && nums[i] % 2 == nums[i+2] % 2) return true;
+			if(nums[i] % 2 == 0 && nums[i+1] % 2 == 0 && nums[i+2] % 2 == 0 ) return true;
+			if(nums[i] % 2 != 0 && nums[i+1] % 2 != 0 && nums[i+2] % 2 != 0 ) return true;
+		}
+		return false;
+	}
+
 
 
 
 	
 	public static void main(String[] args) {
-		int[] nums = {3, 1, 4, 5, 2};
-		System.out.println(has12(nums));
+		int[] nums = {2, 4, 2, 5};
+		System.out.println(modThree(nums));
 	}
 
 }
