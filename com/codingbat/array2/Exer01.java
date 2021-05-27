cf997c5dd692faf9cad91093d94910b21bf55d0e
Oda <odairvaz>
2021-05-27 14:25:03 +0200

    add exercise twoTwo

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -387,14 +387,38 @@ public class Exer01 {
 		}
 		return count3 == 3;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given an array of ints, return true if every 2 that appears in the array is next to another 2.
+	 * 
+	 * twoTwo([4, 2, 2, 3]) → true
+	 * twoTwo([2, 2, 4]) → true
+	 * twoTwo([2, 2, 4, 2]) → false
+	 */
+	public static boolean twoTwo(int[] nums) {
+		if (nums.length == 1 && nums[0] == 2)
+			return false;
+
+		if (nums.length >= 2
+				&& ((nums[0] == 2 && nums[1] != 2) || (nums[nums.length - 1] == 2 && nums[nums.length - 2] != 2)))
+			return false;
+
+		for (int i = 1; i <= nums.length - 2; i++) {
+			if (nums[i] == 2 && nums[i - 1] != 2 && nums[i + 1] != 2)
+				return false;
+		}
+
+		return true;
+	}
 
 
 
 
 	
 	public static void main(String[] args) {
-		int[] nums = {3, 4, 3, 3, 4};
-		System.out.println(haveThree(nums));
+		int[] nums = {2, 2};
+		System.out.println(twoTwo(nums));
 	}
 
 }
