77d5a99960c74df5b4fe746859b5866030d3c544
Oda <odairvaz>
2021-05-31 08:52:52 +0200

    add exercise sameEnds

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -411,14 +411,33 @@ public class Exer01 {
 
 		return true;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Return true if the group of N numbers at the start and end of the array are the same. 
+	 * For example, with {5, 6, 45, 99, 13, 5, 6}, the ends are the same for n=0 and n=2, 
+	 * and false for n=1 and n=3. You may assume that n is in the range 0..nums.length inclusive.
+	 * 
+	 * sameEnds([5, 6, 45, 99, 13, 5, 6], 1) → false
+	 * sameEnds([5, 6, 45, 99, 13, 5, 6], 2) → true
+	 * sameEnds([5, 6, 45, 99, 13, 5, 6], 3) → false
+	 */
+	public static boolean sameEnds(int[] nums, int len) {
+		for (int i = 0; i < len; i++) {
+			if (nums[i] != nums[nums.length - len + i])
+				return false;
+		}
+		return true;
+	}
 
 
 
 
 	
 	public static void main(String[] args) {
-		int[] nums = {2, 2};
-		System.out.println(twoTwo(nums));
+		int[] nums = {5, 6, 45, 99, 13, 5, 6};
+		System.out.println(sameEnds(nums, 1));
+		
 	}
 
 }
