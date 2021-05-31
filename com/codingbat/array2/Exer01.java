3da76db9980690a754c9578f1e456ab041a96e53
Oda <odairvaz>
2021-05-31 09:27:04 +0200

    add exercise fizzArray3

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -446,16 +446,29 @@ public class Exer01 {
 		}
 		return false;
 	}
-
-
-
-
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given start and end numbers, return a new array containing the sequence of integers 
+	 * from start up to but not including end, so start=5 and end=10 yields {5, 6, 7, 8, 9}. 
+	 * The end number will be greater or equal to the start number. 
+	 * Note that a length-0 array is valid. (See also: FizzBuzz Code)
+	 * 
+	 * fizzArray3(5, 10) → [5, 6, 7, 8, 9]
+	 * fizzArray3(11, 18) → [11, 12, 13, 14, 15, 16, 17]
+	 * fizzArray3(1, 3) → [1, 2]
+	 */
+	public static int[] fizzArray3(int start, int end) {
+		int[] result = new int[end - start];
+		for (int i = start; i < end; i++) {
+			result[i - start] = i;
+		}
+		return result;
+	}
 
 	
 	public static void main(String[] args) {
-		int[] nums = {1, 2, 4};
-		System.out.println(tripleUp(nums));
-		
+		System.out.println(Arrays.toString(fizzArray3(1, 3)));
 	}
 
 }
