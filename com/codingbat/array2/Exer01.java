87ef20720ccb0530267205dd1594390890e1b31f
Oda <odairvaz>
2021-06-02 12:24:21 +0200

    add exercise tenRun

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -484,12 +484,36 @@ public class Exer01 {
 		nums[nums.length - 1] = firstElt;
 		return nums;
 	} 
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * For each multiple of 10 in the given array, change all the values following it to be that 
+	 * multiple of 10, until encountering another multiple of 10. 
+	 * So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+	 * 
+	 * tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
+	 * tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
+	 * tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
+	 */
+	public static int[] tenRun(int[] nums) {
+		boolean modTen = false;
+		int tmp = 0;
 
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] % 10 == 0) {
+				tmp = nums[i];
+				modTen = true;
+			} else if (nums[i] % 10 != 0 && modTen) {
+				nums[i] = tmp;
+			}
+		}
+		return nums;
+	}
 
 	
 	public static void main(String[] args) {
-		int[] nums = {6, 2, 5, 3};
-		System.out.println(Arrays.toString(shiftLeft(nums)));
+		int[] nums = {10, 1, 9, 20};
+		System.out.println(Arrays.toString(tenRun(nums)));
 	}
 
 }
