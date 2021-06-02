b9f6e005ca288ba6e91947cd6bbd38dddbd0d739
Oda <odairvaz>
2021-06-02 13:03:13 +0200

    add execise pre4

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -509,11 +509,37 @@ public class Exer01 {
 		}
 		return nums;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given a non-empty array of ints, return a new array containing the elements from the 
+	 * original array that come before the first 4 in the original array. The original array 
+	 * will contain at least one 4. Note that it is valid in java to create an array of length 0.
+	 * 
+	 * pre4([1, 2, 4, 1]) → [1, 2]
+	 * pre4([3, 1, 4]) → [3, 1]
+	 * pre4([1, 4, 4]) → [1]
+	 */
+	public static int[] pre4(int[] nums) {
+		if (nums[0] == 4)
+			return new int[0];
+		
+		int count = 0;
+		while(nums[count] != 4)
+			count++;
+		
+		int[] res = new int[count];
+		for (int i = 0; i < count; i++) {
+			res[i] = nums[i];
+		}
+		return res;
+	}
+
 
 	
 	public static void main(String[] args) {
-		int[] nums = {10, 1, 9, 20};
-		System.out.println(Arrays.toString(tenRun(nums)));
+		int[] nums = { 4, 4};
+		System.out.println(Arrays.toString(pre4(nums)));
 	}
 
 }
