c128847f65b001835aa95042c8ac5b8a100f3333
Oda <odairvaz>
2021-06-07 12:39:09 +0200

    add exercise zeroFront

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -574,13 +574,35 @@ public class Exer01 {
 		}
 		return nums;
 	}
-
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Return an array that contains the exact same numbers as the given array, but rearranged so that 
+	 * all the zeros are grouped at the start of the array. The order of the non-zero numbers does 
+	 * not matter. So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify and return the given array 
+	 * or make a new array.
+	 * 
+	 * zeroFront([1, 0, 0, 1]) → [0, 0, 1, 1]
+	 * zeroFront([0, 1, 1, 0, 1]) → [0, 0, 1, 1, 1]
+	 * zeroFront([1, 0]) → [0, 1]
+	 */
+	public static int[] zeroFront(int[] nums) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				nums[i] = nums[count];
+				nums[count] = 0;
+				count++;
+			}
+		}
+		return nums;
+	}
 		
 	
 	
 	public static void main(String[] args) {
-		int[] nums = { 1, 1, 1, 2 };
-		System.out.println(Arrays.toString(notAlone(nums, 1)));
+		int[] nums = { 1, 0, 0, 1 };
+		System.out.println(Arrays.toString(zeroFront(nums)));
 	}
 
 }
