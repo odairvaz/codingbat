7c66ef2823f28451512b7f8fd020c9c57752358a
Oda <odairvaz>
2021-06-07 09:29:17 +0200

    add exercise post4

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -534,12 +534,34 @@ public class Exer01 {
 		}
 		return res;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given a non-empty array of ints, return a new array containing the elements from the 
+	 * original array that come after the last 4 in the original array. The original array 
+	 * will contain at least one 4. Note that it is valid in java to create an array of length 0.
+	 * 
+	 * post4([2, 4, 1, 2]) → [1, 2]
+	 * post4([4, 1, 4, 2]) → [2]
+	 * post4([4, 4, 1, 2, 3]) → [1, 2, 3]
+	 */
+	public static int[] post4(int[] nums) {
+		int i = nums.length - 1;
+		while (nums[i] != 4)
+			i--;
 
-
+		int[] arr = new int[nums.length - i - 1];
+		for (int j = 0; j < arr.length; j++) {
+			arr[j] = nums[i + j + 1];
+		}
+		return arr;
+	}
+		
+	
 	
 	public static void main(String[] args) {
-		int[] nums = { 4, 4};
-		System.out.println(Arrays.toString(pre4(nums)));
+		int[] nums = { 2, 4, 1, 2 };
+		System.out.println(Arrays.toString(post4(nums)));
 	}
 
 }
