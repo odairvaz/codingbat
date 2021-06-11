5c1ebe08fa0b48b3c47582f82abf9707003226c0
Oda <odairvaz>
2021-06-11 18:26:26 +0200

    add exercise withoutTen

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -597,12 +597,36 @@ public class Exer01 {
 		}
 		return nums;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Return a version of the given array where all the 10's have been removed. 
+	 * The remaining elements should shift left towards the start of the array as needed, 
+	 * and the empty spaces a the end of the array should be 0. So {1, 10, 10, 2} yields {1, 2, 0, 0}. 
+	 * You may modify and return the given array or make a new array.
+	 * 
+	 * withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
+	 * withoutTen([10, 2, 10]) → [2, 0, 0]
+	 * withoutTen([1, 99, 10]) → [1, 99, 0]
+	 */
+	public static int[] withoutTen(int[] nums) {
+		int[] copy = new int[nums.length];
+		int index = 0;
+
+		for (int i = 0; i < nums.length; i++)
+			if (nums[i] != 10) {
+				copy[index] = nums[i];
+				index++;
+			}
+		return copy;
+	}
+
 		
 	
 	
 	public static void main(String[] args) {
-		int[] nums = { 1, 0, 0, 1 };
-		System.out.println(Arrays.toString(zeroFront(nums)));
+		int[] nums = { 1, 10, 10, 2 };
+		System.out.println(Arrays.toString(withoutTen(nums)));
 	}
 
 }
