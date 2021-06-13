9d191286681538f2e5924bb967ec0abf5ec46a40
Oda <odairvaz>
2021-06-13 12:30:19 +0200

    add zeroMax exercise

--- com/codingbat/array2/Exer01.java
+++ com/codingbat/array2/Exer01.java
@@ -620,6 +620,28 @@ public class Exer01 {
 			}
 		return copy;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Return a version of the given array where each zero value in the array is replaced by 
+	 * the largest odd value to the right of the zero in the array. 
+	 * If there is no odd value to the right of the zero, leave the zero as a zero.
+	 * 
+	 * zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
+	 * zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
+	 * zeroMax([0, 1, 0]) → [1, 1, 0]
+	 */
+	public int[] zeroMax(int[] nums) {
+		int max = 0;
+
+		for (int i = nums.length - 1; i >= 0; i--) {
+			if (nums[i] % 2 != 0)
+				max = Math.max(max, nums[i]);
+			if (nums[i] == 0)
+				nums[i] = max;
+		}
+		return nums;
+	}
 
 		
 	
