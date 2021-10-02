02ad124f67bbe3d6dd4ab32d49b4af27e2ccdb98
Oda <odairvaz>
2021-10-02 17:48:41 +0200

    add exercise scoresAverage

--- com/codingbat/ap1/Exer01.java
+++ com/codingbat/ap1/Exer01.java
@@ -35,12 +35,44 @@ public class Exer01 {
 		}
 		return false;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given an array of scores, compute the int average of the first half and the second half, 
+	 * and return whichever is larger. We'll say that the second half begins at index length/2. 
+	 * The array length will be at least 2. To practice decomposition, write a separate helper method
+	 * int average(int[] scores, int start, int end) { which computes the average of the elements 
+	 * between indexes start..end. Call your helper method twice to implement scoresAverage(). 
+	 * Write your helper method after your scoresAverage() method in the JavaBat text area. 
+	 * Normally you would compute averages with doubles, but here we use ints so the expected results are exact.
+	 * 
+	 * scoresAverage([2, 2, 4, 4]) → 4
+	 * scoresAverage([4, 4, 4, 2, 2, 2]) → 4
+	 * scoresAverage([3, 4, 5, 1, 2, 3]) → 4
+	 * 
+	 */
+	public static int scoresAverage(int[] scores) {
+		int avgFirst = average(scores, 0 , scores.length/2);
+		int avgScnd = average(scores, scores.length/2, scores.length);
+		return avgFirst > avgScnd ? avgFirst : avgScnd;
+	}
+	
+	public static int average(int[] scores, int start, int end) {
+		int sum = 0;
+		int cnt = 0;
+		for (int i = start; i < end; i++) {
+			sum += scores[i];
+			cnt++;
+		}
+		return sum / cnt;
+	}
+
 
 
 	
 	public static void main(String[] args) {
-		int[] nums = {100, 1, 100, 100};
-		System.out.println(scores100(nums));
+		int[] nums = {3, 4, 5, 1, 2, 2};
+		System.out.println(scoresAverage(nums));
 	}
 	
 	
