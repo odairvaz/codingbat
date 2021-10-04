449f6e18457316bdfc532d3c46cb8eebc0ce2c05
Oda <odairvaz>
2021-10-04 19:42:29 +0200

    add exercise wordsCount

--- com/codingbat/ap1/Exer01.java
+++ com/codingbat/ap1/Exer01.java
@@ -66,13 +66,31 @@ public class Exer01 {
 		}
 		return sum / cnt;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given an array of strings, return the count of the number of strings with the given length.
+	 * 
+	 * wordsCount(["a", "bb", "b", "ccc"], 1) → 2
+	 * wordsCount(["a", "bb", "b", "ccc"], 3) → 1
+	 * wordsCount(["a", "bb", "b", "ccc"], 4) → 0 
+	 */
+	public static int wordsCount(String[] words, int len) {
+		int count = 0;
+
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].length() == len)
+				count++;
+		}
+		return count;
+	}
 
 
 
 	
 	public static void main(String[] args) {
-		int[] nums = {3, 4, 5, 1, 2, 2};
-		System.out.println(scoresAverage(nums));
+		String[] words = {"a", "bb", "b", "ccc"};
+		System.out.println(wordsCount(words, 1));
 	}
 	
 	
