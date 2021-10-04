d841647904467ba9b96554c00e97d7174c5f4b38
Oda <odairvaz>
2021-10-04 19:49:43 +0200

    add exercise wordsFront

--- com/codingbat/ap1/Exer01.java
+++ com/codingbat/ap1/Exer01.java
@@ -1,5 +1,7 @@
 package com.codingbat.ap1;
 
+import java.util.Arrays;
+
 public class Exer01 {
 
 	// ----------------------------------------------------------------------------------------------------------
@@ -84,13 +86,29 @@ public class Exer01 {
 		}
 		return count;
 	}
+	
+	// ----------------------------------------------------------------------------------------------------------
+	/*
+	 * Given an array of strings, return a new array containing the first N strings. N will be in the range 1..length.
+	 * 
+	 * wordsFront(["a", "b", "c", "d"], 1) → ["a"]
+	 * wordsFront(["a", "b", "c", "d"], 2) → ["a", "b"]
+	 * wordsFront(["a", "b", "c", "d"], 3) → ["a", "b", "c"]
+	 */
+	public static String[] wordsFront(String[] words, int n) {
+		String[] result = new String[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = words[i];
+		}
+		return result;
+	}
 
 
 
 	
 	public static void main(String[] args) {
-		String[] words = {"a", "bb", "b", "ccc"};
-		System.out.println(wordsCount(words, 1));
+		String[] words = {"a", "b", "c", "d"};
+		System.out.println(Arrays.toString(wordsFront(words, 2)));
 	}
 	
 	
